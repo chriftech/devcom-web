d23709ec4602f739b77d442ddd59a8ba477318fc
Siame <chrifordsiame@gmail.com>
2025-08-13 00:54:03 +0100

    Disable strict mode in production

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import {router} from './router'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AuthContextProvider>
       <RouterProvider router={router} />
     </AuthContextProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
