a30951c7f2445224dae530f6036c0837862995a2
Siame <chrifordsiame@gmail.com>
2025-08-13 02:03:58 +0100

    Design the auth screens

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,31 +2,27 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <React.Fragment>
-      <div className='container grid'>
-        <div>
-          <input type="text" name='email' id='emailField' placeholder='email' />
-        </div>
-        <div className='grid md:grid-col-2 lg:grid-col-2'>
-          <div>
-            <input type="text" name='first_name' id='firstNameField' placeholder='first_name' />
+      <div className="w-full flex justify-center py-10 border items-center">
+        <div className='grid gap-3 border rounded-md p-2 w-[95%] md:lg:w-[25%]'>
+          <div className='flex justify-center'>
+          <p className='text-[16pt] font-bold text-gray-600'>Login</p>
           </div>
           <div>
-            <input type="text" name='last_name' id='lastNameField' placeholder='last_name' />
+            <input className="w-full border border-gray-300 px-2 py-1 text-gray-700  " type="text" name='email' id='emailField' placeholder='Email address' />
           </div>
-        </div>
-        <div>
-          <input type="password" name='password' id='passwordField' placeholder='password' />
-        </div>
-        <div>
-          <input type="password" name='password2' id='password2Field' placeholder='Re-enter password' />
-        </div>
-        <div>
-          <p>already have an account?</p>
-          <button>Signup</button>
+          <div>
+            <input className="w-full border border-gray-300 px-2 py-1 text-gray-700  " type="password" name='password' id='passwordField' placeholder='password' />
+          </div>
+          
+          <div className='flex justify-center w-full -mt-1'>
+            <button className='bg-blue-500 text-white w-full py-1 font-semibold'>Login</button>
+          </div>
+          <div className='flex gap-2 justify-between'>
+            <a href='/signup' className='text-blue-500 text-[10pt]'>Signup</a>
+            <a href='#/forgot-password' className='text-blue-500 text-[10pt]'>Forgot password?</a>
+          </div>
         </div>
       </div>
-    </React.Fragment>
   )
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,20 +2,39 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <React.Fragment>
-      <div className='container grid'>
-        <div>
-          <input type="text" name='email' id='emailField' placeholder='email' />
-        </div>
-        <div>
-          <input type="password" name='password' id='passwordField' placeholder='password' />
-        </div>=
-        <div>
-          <p>already have an account?</p>
-          <button>Login</button>
+      <div className="w-full flex justify-center py-10 border items-center">
+        <div className='grid gap-3 border rounded-md p-2'>
+          <div className='flex justify-center'>
+          <p className='text-[16pt] font-bold text-gray-600'>Signup</p>
+          </div>
+          <div>
+            <input className="w-full border border-gray-300 px-2 py-1 text-gray-700  " type="text" name='email' id='emailField' placeholder='Email address' />
+          </div>
+          <div className='grid gap-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-2'>
+            <div>
+              <input className="border border-gray-300 px-2 py-1 text-gray-700  " type="text" name='first_name' id='firstNameField' placeholder='First name' />
+            </div>
+            <div>
+              <input className="border border-gray-300 px-2 py-1 text-gray-700  " type="text" name='last_name' id='lastNameField' placeholder='Last name' />
+            </div>
+          </div>
+          <div>
+            <input className="w-full border border-gray-300 px-2 py-1 text-gray-700  " type="text" name='middle_name' placeholder='Middle name(optional)' />
+          </div>
+          <div>
+            <input className="w-full border border-gray-300 px-2 py-1 text-gray-700  " type="password" name='password' id='passwordField' placeholder='password' />
+          </div>
+          <div>
+            <input className="w-full border border-gray-300 px-2 py-1 text-gray-700  " type="password" name='password2' id='password2Field' placeholder='Re-enter password' />
+          </div>
+          <div className='flex justify-center w-full -mt-1'>
+            <button className='bg-blue-500 text-white w-full py-1 font-semibold'>Signup</button>
+          </div>
+          <div className='flex gap-2 justify-center'>
+            <p className='text-gray-400'>Already have an account? </p><a href='/signin' className='text-blue-500'>Login</a>
+          </div>
         </div>
       </div>
-    </React.Fragment>
   )
 }
 
