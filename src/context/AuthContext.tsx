6f2c24917adf45ea2959285bbe3bd2039e9a388c
Siame <chrifordsiame@gmail.com>
2025-08-27 09:41:13 +0100

    Modify app context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,72 @@
 import React, { useEffect, useState, createContext, useContext } from "react";
+import { supabase } from "../supabaseClient";
 
-const AuthContext = createContext<{ session: string }>({ session: 'undefined' })
+const AuthContext = createContext<{ 
+    session: string, 
+    signIn: () => void | undefined, 
+    signUp: () => void | undefined, 
+} | any>(null)
 
-export const AuthContextProvider: React.FC<React.PropsWithChildren<{}>> = ({children}) => {
-    const [session] =  useState('undefined')
+export const AuthContextProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+    const [session, setSession] = useState<any>(undefined)
+
+    // Signup
+    const SignUp = async ({ email, password}: {email: string, password: string}) => {
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password
+        })
+        if (error) {
+            console.log("Error signing up:", error);
+            const response = { success: false, error }
+            return response
+        }
+        const response = { success: true, data }
+        return response
+    }
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+        });
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        });
+    }, []);
+
+    // Sign in
+    const SignIn = async ({ email, password}: {email: string, password: string}) => {
+        try {
+            const {data, error} = await supabase.auth.signInWithPassword({
+                email: email,
+                password: password
+            })
+            if(error){
+                const result = {success: false, error}
+                console.error("Sign in error occured: ", error)
+                return result
+            }
+            // console.log("Sign-in success: ", data)
+            const result = {success: true, data}
+            return result
+        } catch (error) {
+            console.error("An error occured: ", error);
+        }
+    }
+    // Sign Out
+    const SignOut = async () => {
+        const { error } = await supabase.auth.signOut();
+        if (error) {
+            console.error("there was an error: ", error);
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{session}}>
+        <AuthContext.Provider value={{ 
+            session,
+            SignUp,
+            // SignIn,
+        }}>
             {children}
         </AuthContext.Provider>
     )
